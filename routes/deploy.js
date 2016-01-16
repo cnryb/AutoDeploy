199cf872509c94efaebdfa39227def8c3420ff80
任跃兵 <cnryb@live.cn>
2016-01-16 20:04:20 +0800

    nodegit

--- routes/deploy.js
+++ routes/deploy.js
@@ -5,14 +5,22 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var num=new Date().getUTCSeconds();
-    
+    // var num = new Date().getUTCSeconds();
+
     res.render('index', { title: 'Express' });
 });
 
 router.post('/', function (req, res, next) {
     // JSON.stringify(req)
-    fs.writeFileSync(path.join("./", "req"+new Date().getTime()), JSON.stringify(req));
+    
+    
+    var query = req.body.hook;
+    
+    if (query && query.password === "password" && query.hook_name === "push_hooks") {
+
+    }
+
+    // fs.writeFileSync(path.join("./", "req" + new Date().getTime()), JSON.stringify(req.body));
     res.render('index', { title: 'Express' });
 });
 
